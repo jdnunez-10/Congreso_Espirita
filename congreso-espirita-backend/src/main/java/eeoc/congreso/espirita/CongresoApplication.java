f11738b8eb1d12ac21671f9e625013fd5754a0c0
jdnunez-10 <jdnunezo@unah.hn>
2026-04-21 20:36:08 -0600

    Proyecto Congreso Espirita

---
+++ congreso-espirita-backend/src/main/java/eeoc/congreso/espirita/CongresoApplication.java
@@ -0,0 +1,13 @@
+package eeoc.congreso.espirita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongresoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongresoApplication.class, args);
+	}
+
+}
---
+++ congreso-espirita-backend/src/test/java/eeoc/congreso/espirita/CongresoApplicationTests.java
@@ -0,0 +1,13 @@
+package eeoc.congreso.espirita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongresoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
